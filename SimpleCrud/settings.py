5b071abb1fab3cecbc3c717321dc9d668dbd2dd5
merengo <marwamerengo@gmail.com>
2022-04-25 01:21:16 -0700

    created and modified apps and their migrations

--- SimpleCrud/settings.py
+++ SimpleCrud/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'simple_app',
+    'crud'
+   
+
 ]
 
 MIDDLEWARE = [
@@ -75,9 +79,16 @@ WSGI_APPLICATION = 'SimpleCrud.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'default': {  
+        'ENGINE': 'django.db.backends.mysql',  
+        'NAME': 'simple_db',  
+        'USER': 'root',  
+        'PASSWORD': '',  
+        'HOST': '127.0.0.1',  
+        'PORT': '3306',  
+        'OPTIONS': {  
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
+        }  
     }
 }
 
